1813dc0e75e5845f989ba4299606771c133638a3
haozi007 <liuhao27@huawei.com>
2021-04-15 15:05:48 +0800

    add license to all tools
    Signed-off-by: haozi007 <liuhao27@huawei.com>

--- deploy/tools/new_kubeconfig.sh
+++ deploy/tools/new_kubeconfig.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+#######################################################################
+##- @Copyright (C) Huawei Technologies., Ltd. 2021. All rights reserved.
+# - eggo licensed under the Mulan PSL v2.
+# - You can use this software according to the terms and conditions of the Mulan PSL v2.
+# - You may obtain a copy of Mulan PSL v2 at:
+# -     http://license.coscl.org.cn/MulanPSL2
+# - THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
+# - IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
+# - PURPOSE.
+# - See the Mulan PSL v2 for more details.
+##- @Description: eggo kubeconfig generator tool
+##- @Author: haozi007
+##- @Create: 2021-04-10
+#######################################################################
 
 source ./helper.sh
 
